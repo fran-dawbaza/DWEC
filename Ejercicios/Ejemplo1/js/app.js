61496b47c1e5736e5a5da0ee8123a412e134a1ff
fran-dawbaza <fran@iespedrojimenezmontoya.es>
2020-09-23 18:28:45 +0200

    ejercicio 1 con comentarios

--- Ejercicios/Ejemplo1/js/app.js
+++ Ejercicios/Ejemplo1/js/app.js
@@ -1,33 +1,36 @@
-let imgOriginal=true;
-
-function holaMundo(){
+// Esta función muestra un mensaje en consola y cambiam el contenido del cuerpo o body
+function holaMundo() {
     console.log("Hola Mundo");
     document.write("Holaaaaa!");
     document.write('<br /><a href="index.html">Recupera el contenido</a>');
 }
 
-function modificaH2(){
+// Esta función modifica el contenido HTML dentro de una etiqueta
+function modificaH2() {
     //let variable = document.getElementById("h2");
     //console.log(variable);
     //variable.innerHTML="contenido cambiado";
-    document.getElementById("h2").innerHTML="contenido modificado!!!";
+    document.getElementById("h2").innerHTML = "contenido modificado!!!";
 }
 
-function cambiaImg(){
+
+// Esta función modifica el atributo src de una imagen, dando efecto de conmutación entre dos imágenes
+// Usamos la variable booleana imgOriginal para saber si está mostrándose la imagen original
+let imgOriginal = true;
+
+function cambiaImg() {
     if (imgOriginal) {
-        document.getElementById("gatitos").src="https://cf.ltkcdn.net/gatos/images/std/236641-699x450-etapas-desarrollo-gatitos.jpg";
-        imgOriginal=false;
-    }
-    else {
-        document.getElementById("gatitos").src="https://www.purina.es/sites/g/files/mcldtz1656/files/2017-11/Bringing-Your-Kitten-Home_0.jpg";
-        imgOriginal=true;
+        document.getElementById("gatitos").src = "https://cf.ltkcdn.net/gatos/images/std/236641-699x450-etapas-desarrollo-gatitos.jpg";
+        imgOriginal = false;
+    } else {
+        document.getElementById("gatitos").src = "https://www.purina.es/sites/g/files/mcldtz1656/files/2017-11/Bringing-Your-Kitten-Home_0.jpg";
+        imgOriginal = true;
     }
 }
 
-function cambiaEstilo(){
+// Esta función modifica el estilo de un párrafo
+function cambiaEstilo() {
     let miTexto = document.getElementById("texto");
-    miTexto.style.fontSize="24px";
-    miTexto.style.color="red";
+    miTexto.style.fontSize = "24px";
+    miTexto.style.color = "red";
 }
-
-document.getElementsByTagNameNS()
