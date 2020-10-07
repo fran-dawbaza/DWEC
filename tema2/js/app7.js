69d300d5a5db8ffe99dcbc75d963acd3adf86db6
fran-dawbaza <fran@iespedrojimenezmontoya.es>
2020-10-07 12:46:46 +0200

    actualizado 7 oct

--- tema2/js/app7.js
+++ tema2/js/app7.js
@@ -43,14 +43,25 @@ function Coche(fabricante, modelo, ejercicio) {
     this.modelo = modelo
     this.ejercicio = ejercicio
 }
+
 miCoche = new Coche("Honda", "Accord", 1998)
+
 a = miCoche instanceof Coche // devuelve verdadero (true)
 b = miCoche instanceof Object // devuelve verdadero (true)
 
 console.log(a, b);
 
+const f_evento = function(e) {
+    e.preventDefault(); // Detiene el comportamiento "normal" del evento
+    console.log(e); // Objeto que dispara el evento
+}
+
+// Devuelve el primer objeto de tipo párrafo que encuentre
 let p = document.querySelector('p');
-p.addEventListener('click', function(e) {
-    e.preventDefault();
-    console.log(e.target);
-});
+
+// Asociamos al evento click la función f_evento
+p.addEventListener('click', f_evento);
+
+/*
+document.querySelector('p').addEventListener('click', function(e) { e.preventDefault(); console.log(e);});
+*/
