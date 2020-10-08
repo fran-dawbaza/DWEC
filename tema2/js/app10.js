0ba5fb5646508c10ae366e39956bcff53c12aec7
fran-dawbaza <fran@iespedrojimenezmontoya.es>
2020-10-08 19:46:47 +0200

    actualizado oct 8

--- tema2/js/app10.js
+++ tema2/js/app10.js
@@ -1,4 +1,4 @@
-const salida = document.getElementById('salida');
+/*
 let step;
 
 for (step = 0; step < 5; step++) {
@@ -10,4 +10,42 @@ try {
     salida.innerHTML += '<br>Ya me cansé, he caminado ' + step + ' pasos';
 } catch {
     salida.innerHTML += '<br>Sucedió un error!'
-}
+}*/
+
+const dibujaRombo = evento => {
+    
+    evento.preventDefault();
+
+    const tope = Number(document.getElementById('tamanio').value);
+    const salida = document.getElementById('salida');
+
+    if (isNaN(tope) || tope < 1 || tope > 100){
+        salida.innerHTML="Error, el tamaño indicado debe ser número entre 1 y 99";
+        salida.style.color='red';
+        return;
+    }
+    salida.style.color='blue';
+    salida.innerHTML='';
+
+    const espacio='&nbsp;';
+    const asterisco='*';
+
+    let num_espacios=tope-1;
+
+    // Si véis esta solución, implementar otra que no use el método repeat
+    // así os obligo a usar bucles anidados
+
+    for (let i=0; i<tope;i++){
+        salida.innerHTML += espacio.repeat(num_espacios--);
+        salida.innerHTML += asterisco.repeat(1+i*2);
+        salida.innerHTML += '<br>';
+    }
+    num_espacios=1;
+    for (let i=tope-2; i>=0;i--){
+        salida.innerHTML += espacio.repeat(num_espacios++);
+        salida.innerHTML += asterisco.repeat(1+i*2);
+        salida.innerHTML += '<br>';
+    }
+};
+
+document.getElementById('pinta').addEventListener('click',dibujaRombo);
