cd481fcf96c7357466975e7f3df4cfbbe67f99d9
fran-dawbaza <fran@iespedrojimenezmontoya.es>
2020-11-04 18:20:28 +0100

    actualizado nov 4

--- Tema3/js/app09.js
+++ Tema3/js/app09.js
@@ -1,4 +1,8 @@
+// con console.time y console.timeEnd mostramos el tiempo trascurrido entre esas 2 llamadas
+
 console.time("tiempo de carga del diccionario");
+// diccionario.js sólo contiene la declaración del array diccionario y su exportación
+// a continuación importamos el array diccionario
 import { diccionario } from './diccionario.js';
 console.timeEnd("tiempo de carga del diccionario");
 
