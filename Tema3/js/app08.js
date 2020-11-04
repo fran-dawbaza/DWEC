494641f635ee16b7352f6c51ab60cdbc17cff9a8
fran-dawbaza <fran@iespedrojimenezmontoya.es>
2020-11-04 18:15:27 +0100

    actualizado nov 4

--- Tema3/js/app08.js
+++ Tema3/js/app08.js
@@ -112,10 +112,3 @@ console.log(unCocheES6);
 console.log(otroCocheES6.toString());
 
 console.log(JSON.stringify(unCocheES6));
-
-
-
-import { diccionario } from './diccionario.js';
-
-console.log("hay " + diccionario.length + " palabras en el diccionario");
-console.log(diccionario);
