e8b7ed88ddc9b1b5a4983459bcb89a6cd8b69ce3
fran-dawbaza <fran@iespedrojimenezmontoya.es>
2021-02-18 00:20:03 +0100

    Actualizado feb 2

--- tema5/js/componentes/jsontable/index.js
+++ tema5/js/componentes/jsontable/index.js
@@ -1,19 +1,19 @@
 class JsonTable extends HTMLElement {
     constructor() {
         super();
-        this.attachShadow({mode: 'open'});
+        this.attachShadow({ mode: 'open' });
         this.rendered = false;
     }
     connectedCallback() {
-        if (!this.rendered){
-            console.log('render llamado desde connected');            
+        if (!this.rendered) {
+            console.log('render llamado desde connected');
             //this.render();
         }
     }
 
     render() {
         //implementation
-        let html='<link rel="stylesheet" href="js/componentes/jsontable/styles.css"><table><thead><tr>';
+        let html = '<link rel="stylesheet" href="js/componentes/jsontable/styles.css"><table><thead><tr>';
         console.log('datos[0]:' + Array.isArray(this.datos[0]));
         console.log('datos:' + Array.isArray(this.datos));
         let primerObjeto;
@@ -21,41 +21,67 @@ class JsonTable extends HTMLElement {
         if (Array.isArray(this.datos)) {
             primerObjeto = this.datos[0];
             arrayObjetos = this.datos;
-        }
-        else {
+        } else {
             primerObjeto = this.datos;
             arrayObjetos = [this.datos];
         }
-       
+
         for (const propiedad of Object.keys(primerObjeto)) {
-         html += `<th>${propiedad}</th>`;
+            html += `<th>${propiedad}</th>`;
         }
         html += '</tr></thead><tbody>';
 
         for (const usuario of arrayObjetos) {
             html += '<tr>';
-            
+
             for (const valor of Object.values(usuario)) {
-                if (typeof valor==='object') {
-                    html += this.renderTable(valor);
-                }
-                else {
+                if (typeof valor === 'object') {
+                    html += `<td>${this.renderTable(valor)}</td>`;
+                } else {
                     html += `<td>${valor}</td>`;
                 }
             }
 
-            html+='</tr>';
+            html += '</tr>';
         }
 
         html += '</tbody></table>';
 
 
-        this.shadowRoot.innerHTML=html;
-        this.rendered=true;
+        this.shadowRoot.innerHTML = html;
+        this.rendered = true;
     }
 
     renderTable(obj) {
-        return `<td>un objeto</td>`;
+            let primerObjeto;
+            let arrayObjetos;
+            if (Array.isArray(obj)) {
+                primerObjeto = obj[0];
+                arrayObjetos = obj;
+            } else {
+                primerObjeto = obj;
+                arrayObjetos = [obj];
+            }
+            return `
+            <button>-</button> ${Object.values(primerObjeto)[0]}
+            <table>
+                <thead>
+                    <tr>
+                        ${Object.keys(primerObjeto).map(key => `<th>${key}</th>`).join('')}
+                    </tr>
+                </thead>
+                <tbody>
+                    ${arrayObjetos.map(
+                                        obj => `<tr>${
+                                                        Object.values(obj).map(
+                                                                                value => `<td>${typeof value === 'object' ? this.renderTable(value) : value}</td>`
+                                                                                ).join('')
+                                                    }
+                                                </tr>`
+                                        ).join('')
+                    }
+                </tbody>
+            </table>`;
     }
 
 
